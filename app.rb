6829b341be6ede574db84098bb1e336dcaa02b71
Max Rogers <maxrogers90@gmail.com>
2014-05-17 19:24:30 -0400

    asdf

--- app.rb
+++ app.rb
@@ -8,3 +8,11 @@ get '/' do
 end
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+class Fact
+	include DataMapper::Resource
+	property :id, Serial
+	property :name, String, :length => 25
+end	
+
+DataMapper.auto_upgrade!
