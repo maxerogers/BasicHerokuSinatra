a265629869c0b7f705229d6a6b23c55b1b0d8fd0
Max Rogers <maxrogers90@gmail.com>
2014-05-17 19:14:48 -0400

    adsf

--- Gemfile
+++ Gemfile
@@ -4,7 +4,3 @@ source 'https://rubygems.org'
 ruby '2.1.1'
 
 gem 'sinatra'
-gem 'data_mapper'
-gem 'slim'
-gem 'tux'
-gem 'dm-postgres-adapter'
--- app.rb
+++ app.rb
@@ -1,24 +1,8 @@
 require 'sinatra'
-require 'slim'
-require 'dm-postgres-adapter'
-require 'data_mapper'
 
 get '/' do
-	@fact = Fact.first(:offset => rand(Fact.count))
-	@x = "1"
-	slim :index
+  "Hello World!"
 end
 
+require 'data_mapper'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-class Fact
-  include DataMapper::Resource
-  property :id,           Serial
-  property :data,         String, :required => true, length: 255
-end
-
-DataMapper.finalize
-Fact.auto_upgrade!
-
-#seed
-require './seeds'
